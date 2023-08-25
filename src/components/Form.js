f9f8dbbc274891bd2bd16c2c6011275e97b2011e
tajemouti <tajemouti@gmail.com>
2023-08-25 12:39:51 +0100

    Add form component

---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+const Form = () => (
+  <form>
+    <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Author" />
+    <button type="submit">ADD BOOk</button>
+  </form>
+);
+
+export default Form;

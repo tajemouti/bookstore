966fb8881e970ebf86bd3b6ffb74b7dd2f74e6f5
tajemouti <tajemouti@gmail.com>
2023-08-25 13:00:46 +0100

    Add routes inside App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import BooksPage from './components/BooksPage';
+import Categories from './components/Categories';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Bookstore!</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<BooksPage />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 

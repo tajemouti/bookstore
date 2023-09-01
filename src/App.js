d8ba3118ef5ae2aeb9944e279ab15133689f29ac
tajemouti <tajemouti@gmail.com>
2023-09-01 02:16:28 +0100

    Fetched Books on load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import BooksPage from './components/BooksPage';
 import Categories from './components/Categories';
+import { fetchInitialBooks } from './redux/books/booksSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchInitialBooks());
+  }, [dispatch]);
+
   return (
     <>
       <Header />

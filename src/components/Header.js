28f51e9628f9e2abc771812e44588b1e55c112fe
tajemouti <tajemouti@gmail.com>
2023-09-02 02:28:33 +0100

    Style the form element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  <header>
-    <nav className="navbar">
-      <h2 className="logo">Bookstore CMS</h2>
-      <ul>
-        <li className="books-link">
-          <NavLink to="/">BOOKS</NavLink>
-        </li>
-        <li className="categories">
-          <NavLink to="/categories">CATEGORIES</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <div>
-      <img src="" alt="Avatar" />
-    </div>
-  </header>
+  <>
+    <header>
+      <nav className="navbar">
+        <h2 className="logo">Bookstore CMS</h2>
+        <ul>
+          <li className="books-link">
+            <NavLink to="/">BOOKS</NavLink>
+          </li>
+          <li className="categories">
+            <NavLink to="/categories">CATEGORIES</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <img src="" alt="Avatar" />
+      </div>
+    </header>
+    <hr className="line" />
+  </>
 );
 
 export default Header;
